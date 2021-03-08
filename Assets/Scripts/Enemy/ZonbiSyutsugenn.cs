c7458e59531dbcf6057b7de3f7351b6e455d276b
onfuu <mbsskk1221@gmail.com>
2021-03-08 21:07:39 +0900

    Enemyギミック

---
+++ Assets/Scripts/Enemy/ZonbiSyutsugenn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonbiSyutsugenn : MonoBehaviour
+{
+
+    public GameObject zonbi;
+
+    private void Start()
+    {
+        zonbi.SetActive(false);
+
+    }
+    //public GameObject player;
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") == true)
+        {
+            zonbi.SetActive(true);
+        }
+
+    }
+}

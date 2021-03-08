4dea1e9e838795c37a34a896dfa2fb61c22ccf4d
YunaTakahashi-7 <76086684+YunaTakahashi-7@users.noreply.github.com>
2021-03-08 11:37:38 +0900

    In BossScene, added flashing on damage

--- Assets/Scripts/Boss/Bosshp.cs
+++ Assets/Scripts/Boss/Bosshp.cs
@@ -7,16 +7,24 @@ public class Bosshp : MonoBehaviour
     public int bosshp = 20;
     private int halfhp;
     public GameObject Zangeki;
+    private SpriteRenderer renderer;　//点滅用
+    private bool on_damage = false;
     
     void Start()
     {
         halfhp = bosshp / 2;
+        renderer = gameObject.GetComponent<SpriteRenderer>(); //点滅用
     }
 
     void Update()
     {
         if(bosshp <= halfhp){
             transform.Rotate(new Vector3(0, 0, 3));
+        }
+        // ダメージフラグがtrueで有れば点滅
+        if(on_damage){
+            float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
+            renderer.color = new Color(1f,1f,1f,level);
         }
     }
 
@@ -25,9 +33,25 @@ public class Bosshp : MonoBehaviour
         if (collision.CompareTag("Sword") == true)
         {
             Instantiate(Zangeki, transform.position, transform.rotation);
-            transform.position += new Vector3(0.3f, 0.3f, 0);
-
+            // transform.position += new Vector3(0.1f, 0.1f, 0);
             bosshp--;
+        }else if (collision.CompareTag("Bullet") == true && !on_damage)
+        {
+            // Instantiate(Zangeki, transform.position, transform.rotation);
+            // transform.position += new Vector3(0.1f, 0.1f, 0);
+            bosshp--;
+            on_damage = true;
+            StartCoroutine("WaitForIt");
         }
+    }
+
+    IEnumerator WaitForIt()
+    {
+        // 1秒間処理を止める
+        yield return new WaitForSeconds(1);
+
+        // １秒後ダメージフラグをfalseにして点滅を戻す
+        on_damage = false;
+        renderer.color = new Color(1f,1f,1f,1f);
     }
 }

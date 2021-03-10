b054179b4677fdec2ed2ce9394d8201ca9d9abd3
YunaTakahashi-7 <76086684+YunaTakahashi-7@users.noreply.github.com>
2021-03-10 23:57:51 +0900

    added opening movie to BossScene

---
+++ Assets/Scripts/Boss/BossCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCamera : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.enabled = false;
+        StartCoroutine("AudioOn");
+    }
+
+    IEnumerator AudioOn()
+    {
+        yield return new WaitForSeconds(5f);
+        audioSource.enabled = true;
+    }
+}
